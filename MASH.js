c612cda8903173361335fc970c3a63f73432738f
IziahASC6 <iziahhouse@gmail.com>
2019-08-02 14:26:26 -0400

    MASH

---
+++ MASH.js
@@ -0,0 +1,17 @@
+function mash(){
+   let word = "house"
+ return word
+}
+answer=mash()
+console.log(answer)
+
+function getHome(){
+    homes = ["mansion","apartment", "house", "Shack"]
+    let pick = Math.random() * homes.length;
+    let interger =Math.floor(pick);
+    return homes[intenger];
+}
+
+answer =getHome()
+console.log(getHome)
+
---
+++ mash.1.js
@@ -0,0 +1,3 @@
+function mash() {
+    return mash;
+}
